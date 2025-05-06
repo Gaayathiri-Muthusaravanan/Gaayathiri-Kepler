909579ee157a6529741d14c274c453b6ccd89f3e
Gaayathiri <gaayathirigeetha@gmail.com>
2025-05-06 13:51:03 -0400

    js added, created footer and skills

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-const footer = document.createElement("footer");
+const footer = document.getElementById("footer");
 const container = document.querySelector(".container");
 container.appendChild(footer);
 footer.style.textAlign="center";
 footer.style.backgroundColor="#2c3e50";
 footer.style.color="white";
+footer.style.padding = "20px";
 footer.innerHTML = "Copyrights";
 const today = new Date();
 const thisYear = today.getFullYear();
 footer.innerHTML = `&copy; Gaayathiri Muthusaravanan ${thisYear} — All rights reserved.`;
-const skillArray = ["Java", "Java script", "HTML", "CSS"];
+const skillArray = ["Java", "Java script", "HTML", "CSS", "Github"];
 const skillSection = document.getElementById("skills");
-const skillList = document.createElement("ul");
-skillList.className = "skill-list";
+const skillList = document.getElementById("skillList");s
 for(let i=0; i<skillArray.length; i++){
     const skillItem = document.createElement("li");
     skillItem.className = "skill-item";
